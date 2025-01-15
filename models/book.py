a09b8dc0318e76576e6862677c171b64eb970d49
Hay1tov <eldorh35@gmail.com>
2025-01-15 10:49:32 +0500

    fixed

--- models/book.py
+++ models/book.py
@@ -1,7 +1,5 @@
-from .user import User
-from .room import Room
-
 class Book:
-    def __init__(self, user: User, room: Room):
-        pass
-    
+    def __init__(self, type: str, rooms: int, price: str):
+        self.turi = type
+        self.xonalari = rooms
+        self.narxi = price
