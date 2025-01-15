ea70f7a066b311f0947ed8c40831d19afc320067
IkramovAmir <amirikromov30@gmail.com>
2025-01-15 10:44:56 +0500

    fixed

--- models/user.py
+++ models/user.py
@@ -1,4 +1,12 @@
 class User:
     def __init__(self, name: str, age: int, username: str, password: str) -> None:
-        pass
-    
+        self.name = name
+        self.age = age
+        self.username = username
+        self.password = password
+        
+    def getter(self):
+        self.name = input("Enter name: ")
+        self.age = int(input("Enter your age: "))
+        self.username = input("Enter Username: ")
+        self.password = ("Enter password: ")
